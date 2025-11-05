ccedc68d6147c0c3c5cc66d73ffbb09882148114
Suhassk205 <suhassk2005@gmail.com>
2025-11-05 23:10:29 +0530

    fix: correct Firebase import path case sensitivity

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -7,7 +7,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
-import auth from "../firebase/firebase.config";
+import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
